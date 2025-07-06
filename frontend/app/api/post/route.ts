dd779c8dde3329d51847f1788212b040b1712e70
HossyWorlds <hta43435@gmail.com>
2025-07-07 00:46:53 +0900

    add: post api

--- frontend/app/api/post/route.ts
+++ frontend/app/api/post/route.ts
@@ -6,3 +6,18 @@ export async function GET() {
     const allBBSPost = await prisma.post.findMany();
     return NextResponse.json(allBBSPost);
 }
+
+export async function POST(req: Request) {
+    try {
+        const { title, username, content } = await req.json();
+        if (!title || !username || !content) {
+            return new NextResponse(JSON.stringify({ message: "Missing fields" }), { status: 400 });
+        }
+        const newPost = await prisma.post.create({
+            data: { title, username, content },
+        });
+        return NextResponse.json(newPost, { status: 201 });
+    } catch {
+        return new NextResponse(JSON.stringify({ message: "Server Error" }), { status: 500 });
+    }
+}
